d10c43e32f0602e540304d771100cd163d1d11d5
Muslim <muslim.krum.85@gmail.com>
2018-05-02 00:09:40 +0300

    first component dev

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Store from './store'
+import Axios from 'axios'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 
+let router = new VueRouter ({
+
+})
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
